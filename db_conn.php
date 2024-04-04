7b1cc79bec33bf28a49cd982f1576a89736e6b3d
ADITHYA K.L <160839485+Adithyaaa13@users.noreply.github.com>
2024-04-04 08:46:38 +0530

    Add files via upload

---
+++ db_conn.php
@@ -0,0 +1,38 @@
+<?php
+
+    $server_name ="localhost";
+
+    $username ="root";
+
+    $password = "";
+
+    $database_name ="user_db";
+
+
+
+    //mysqli is a class
+
+
+
+    $conn = new mysqli("$server_name","$username","$password","$database_name");
+
+
+
+    // if ($conn->connect_error) {
+
+    //     # code...
+
+    //     echo "Connection Error";
+
+    // }
+
+    // else{
+
+    //     echo "Connected Success";
+
+    // }
+
+
+
+?>
+
